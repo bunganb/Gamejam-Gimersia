463f2b76fd73606b8940b3ba5a637aadceef8798
bung_anb <bunganabila1005@gmail.com>
2025-11-08 14:52:52 +0700

    change git ignore

--- Assets/Script/Mechanics/GameManager.cs
+++ Assets/Script/Mechanics/GameManager.cs
@@ -1,16 +1,9 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject[] sheep;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
